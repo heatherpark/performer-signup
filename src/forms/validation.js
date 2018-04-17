152f6f5064e6c86d622277af8015300a9f895350
Heather Park <hthr.prk@gmail.com>
2018-04-16 21:30:33 -0700

    Clean up validation code

--- src/forms/validation.js
+++ src/forms/validation.js
@@ -1,6 +1,17 @@
 export const alphanumeric = value => (
   value && /[^a-zA-Z0-9 ]/i.test(value)
-  ? 'Only alphanumeric characters'
-  : undefined);
+  ? 'Invalid character has been detected'
+  : undefined
+);
 
-export const required = value => value ? undefined : 'Required';
+// export const minLength = min => value => (
+//   value && value.length < min ?
+//     `Minimum ${min} characters` : undefined
+// );
+
+export const minLength = min => value => (
+  value && value.length < min ?
+    `Must be a minimum of ${8} characters` : undefined
+);
+  
+export const required = value => value ? undefined : 'This field is required';
