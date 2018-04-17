2fda8f6d627ff472f183b23a1a2f2867666dd548
Heather Park <hthr.prk@gmail.com>
2018-04-16 17:56:40 -0700

    Add AgeVerification component

---
+++ src/AgeVerification.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AgeVerification extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default AgeVerification;
