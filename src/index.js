e73c52e9b4c7ab623ec5903d2015cd4660218b46
Heather Park <hthr.prk@gmail.com>
2018-04-16 17:34:04 -0700

    Set up Redux store

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
