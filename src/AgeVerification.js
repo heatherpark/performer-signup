aed480023a1a7c9bf6ac834c4a1439fba56ebf9e
Heather Park <hthr.prk@gmail.com>
2018-04-16 19:58:23 -0700

    Render form with Redux-Form and implement validation

--- src/AgeVerification.js
+++ src/AgeVerification.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
+const required = value => value ? undefined : 'Required';
+
 class AgeVerification extends Component {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = touched && error ? 'invalid-field' : '';
+
+    return (
+      <div className={className}>
+        <input
+          type="text"
+          {...field.input} />
+        <div className="error-text">
+          {touched ? error : ''}
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
-      <form></form>
+      <form>
+        <Field
+          name="username"
+          validate={required}
+          component={this.renderField} />
+      </form>
     );
   }
 }
