1530321e036d5c30a0ccb5372a162c9090d4c27c
Heather Park <hthr.prk@gmail.com>
2018-04-17 00:54:37 -0700

    Add global styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './style.css';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
