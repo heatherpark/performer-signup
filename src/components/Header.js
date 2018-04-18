0b332e98a86d3b6d6dbe6bd80f3cd3eb5ff8711f
Heather Park <hthr.prk@gmail.com>
2018-04-18 00:19:18 -0700

    Add media queries

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => {
+  return (
+
+  );
+};
+
+export default Header;
--- src/components/SignupFooter.js
+++ src/components/SignupFooter.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './SignupFooter.css';
 
 const SignupFooter = (props) => {
-  console.log(props);
   return (
     <div className="signup-footer">
       <button className="backward">
